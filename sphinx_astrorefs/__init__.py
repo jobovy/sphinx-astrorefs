876bff890e3e4350d8f52a38b84a9bfad34856b6
Jo Bovy <bovy@astro.utoronto.ca>
2020-07-01 01:23:57 -0400

    Add CSS to remove label in HTML reference list

--- sphinx_astrorefs/__init__.py
+++ sphinx_astrorefs/__init__.py
@@ -1,15 +1,22 @@
+from pathlib import Path
 from . import pybtex_astro
 from . import reformat
 from . import resolve_aas
 __version__= "0.1.dev0"
+
+def append_static_path(app,config):
+    config.html_static_path.append(
+        str(Path(__file__).parent.joinpath("_static").absolute()))
 
 def setup(app):
     app.add_config_value('astrorefs_resolve_aas_macros',False,'env')
     app.add_config_value('astrorefs_resolve_aas_macros_infile',None,'env')
     app.add_config_value('astrorefs_resolve_aas_macros_outfile',None,'env')
     app.connect("config-inited",reformat.setup_latex)
+    app.connect("config-inited",append_static_path)
     app.connect("env-before-read-docs",resolve_aas.resolve)
     app.connect("build-finished",reformat.reformat_output)
+    app.add_css_file("sphinx_astrorefs.css")
     pybtex_astro.register()
     return {
         "version": __version__,
