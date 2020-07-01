d85360d7237fcf9397612f3ded7dd46a76af6444
Jo Bovy <bovy@astro.utoronto.ca>
2020-07-01 02:16:23 -0400

    Bump to next dev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sphinx-astrorefs",
-    version="0.1",
+    version="0.2.dev0",
     author="Jo Bovy",
     author_email="bovy@astro.utoronto.ca",
     description="Astro-style references in Sphinx documents",
