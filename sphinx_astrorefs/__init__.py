48b317b56c327beaaa2bd08b24e3b81b14ce2a1c
Jo Bovy <bovy@astro.utoronto.ca>
2020-08-09 13:18:48 -0400

    Bump to next release

--- sphinx_astrorefs/__init__.py
+++ sphinx_astrorefs/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from . import pybtex_astro
 from . import reformat
 from . import resolve_aas
-__version__= "0.3.dev0"
+__version__= "0.3"
 
 def append_static_path(app,config):
     config.html_static_path.append(
