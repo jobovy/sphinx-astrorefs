3071b38753b18cd96fef471abf6a3db70e7c2f1f
Jo Bovy <bovy@astro.utoronto.ca>
2020-08-25 01:37:19 -0400

    Bump version to next development version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ else:
 author = 'Jo Bovy'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4'
+release = '0.5.dev0'
 
 
 # -- General configuration ---------------------------------------------------
