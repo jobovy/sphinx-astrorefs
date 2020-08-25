647a8ebacfb272530930072c11c1f9fce0cb0475
Jo Bovy <bovy@astro.utoronto.ca>
2020-08-25 01:24:15 -0400

    Add remainder of AAS macros

--- sphinx_astrorefs/resolve_aas.py
+++ sphinx_astrorefs/resolve_aas.py
@@ -1,12 +1,12 @@
 # Quick script to replace all AAS macros in a bibtex file
-import sys
 import os.path
-import re
 from sphinx.errors import ExtensionError
 
 aas_macros_dict= {
+    '\\apjsupp' : 'Astrophys. J. Supp.',
     '\\apjs'    : 'Astrophys. J. Supp.',
     '\\appjlett': 'Astrophys. J. Lett.',
+    '\\appjl'   : 'Astrophys. J. Lett.',
     '\\apj'     : 'Astrophys. J.',
     '\\aj'      : 'Astron. J.',
     '\\mnras'   : 'Mon. Not. Roy. Astron. Soc.',
@@ -14,21 +14,55 @@ aas_macros_dict= {
     '\\bain'    : 'Bull. Astron. Inst. Netherlands',
     '\\aapr'    : 'Astron. & Astrophys. Rev.',
     '\\aaps'    : 'Astron. & Astrophys. Supp.',
+    '\\astap'   : 'Astron. & Astrophys.',
     '\\aap'     : 'Astron. & Astrophys.',
     '\\araa'    : 'Ann. Rev. Astron. Astrophys.',
     '\\actaa'   : 'Acta Astronomica',
     '\\apss'    : 'Astrophys. & Space Sci.',
     '\\jcap'    : 'J. Cosmo & Astropart. Phys.',
     '\\nat'     : 'Nature',
+    '\\nar'     : 'New Astron. Rev.',
     '\\na'      : 'New Astron.',
     '\\pra'     : 'Phys. Rev. A',
+    '\\prb'     : 'Phys. Rev. B',
+    '\\prc'     : 'Phys. Rev. C',
     '\\prd'     : 'Phys. Rev. D',
+    '\\pre'     : 'Phys. Rev. E',
     '\\prl'     : 'Phys. Rev. Lett.',
     '\\pasa'    : 'Pub. Astron. Soc. Aus.',
     '\\pasp'    : 'Pub. Astron. Soc. Pac.',
     '\\pasj'    : 'Pub. Astron. Soc. Japan',
     '\\rmxaa'   : 'Rev. Mex. Astron. & Astrofys.',
-    '\\ssr'     : 'Space Sci. Rev.'
+    '\\ssr'     : 'Space Sci. Rev.',
+    '\\applopt' : 'Appl. Opt.',
+    '\\ao'      : 'Appl. Opt.',
+    '\\azh'     : 'Astron. Zhu.',
+    '\\bac'     : 'Bull. Astron. Czech.',
+    '\\caa'     : 'Chin. Astron. Astrophys.',
+    '\\cjaa'    : 'Chin. J. Astron. Astrophys.',
+    '\\icarus'  : 'Icarus',
+    '\\jrasc'   : 'J. RAS Can.',
+    '\\memras'  : 'Mem. RAS',
+    '\\qjras'   : 'Quat. J. RAS',
+    '\\skytel'  : 'Sky & Telescope',
+    '\\solphys' : 'Sol. Phys.',
+    '\\sovast'  : 'Sov. Astron.',
+    '\\zap'     : 'ZeitSch. Astrophys.',
+    '\\iaucirc' : 'IAU Circs.',
+    '\\aplett'  : 'Astrophys. Lett.',
+    '\\apspr'   : 'Astrophys. Space Phys. Res.',
+    '\\fcp'     : 'Fund. Cosm. Phys.',
+    '\\gca'     : 'Geochim. Cosmochim. Acta',
+    '\\grl'     : 'Geophys. Res. Lett',
+    '\\jcp'     : 'J. Chem. Phys.',
+    '\\jgr'     : 'J. Geophys. Res.',
+    '\\jqsrt'   : 'J. Quant. Spec. Rad. Trans.',
+    '\\memsai'  : 'Mem. Soc. Astron. Ital.',
+    '\\nphysa'  : 'Nucl. Phys. A',
+    '\\physrep' : 'Phys. Rep.',
+    '\\physscr' : 'Phys. Scrip.',
+    '\\planss'  : 'Plan. Space. Sci.',
+    '\\procspie': 'Proc. SPIE'
     }
 
 def resolve(app,env,docnames):
