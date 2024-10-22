c70f549e9103ef2c3d2a4944aa822936cc360d4e
Jo Bovy <bovy@astro.utoronto.ca>
2024-10-22 09:23:59 -0400

    Make use of status_iterator compatible again with earlier Sphinx versions

--- sphinx_astrorefs/reformat.py
+++ sphinx_astrorefs/reformat.py
@@ -3,9 +3,14 @@ import os, os.path
 import shutil
 import tempfile
 import re
+from packaging.version import parse as parse_version
+import sphinx
 from sphinx.locale import __
 from sphinx.util import logging
-from sphinx.util.display import status_iterator
+if parse_version(sphinx.__version__) >= parse_version("6.1"):
+    from sphinx.util.display import status_iterator
+else:
+    from sphinx.util import status_iterator
 logger= logging.getLogger(__name__)
 # No sphinxcontrib.bibtex.__version__, so need to version check the hard way...
 _SPHX_BIBTEX_VERSION= 2
